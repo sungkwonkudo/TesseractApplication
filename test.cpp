b9e0fc6b5e9b5c28dea7ee30c8bc9e0cab083b39
sungkwonkudo <sunries.k@gmail.com>
2017-09-06 13:49:42 -0400

    Added test image. Copied stuff around

--- test.cpp
+++ test.cpp
@@ -7,13 +7,13 @@ int main()
 
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, "eng")) {
+    if (api->Init(NULL, "jpn")) {
         fprintf(stderr, "Could not initialize tesseract.\n");
         exit(1);
     }
 
     // Open input image with leptonica library
-    Pix *image = pixRead("/usr/src/tesseract/testing/phototest.tif");
+    Pix *image = pixRead("test.png");
     api->SetImage(image);
     // Get OCR result
     outText = api->GetUTF8Text();
